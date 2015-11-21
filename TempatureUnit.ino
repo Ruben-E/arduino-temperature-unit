e5d7b3a0125bdd6092910032908989edcdfda1ef
Ruben Ernst <rubenernst@gmail.com>
2015-11-21 10:46:50 +0100

    Removed the delay and added a button click handler to enable the backlight of the display

--- TempatureUnit.ino
+++ TempatureUnit.ino
@@ -10,19 +10,25 @@
 #define DHTPIN      2
 #define CEPIN       7
 #define CSNPIN      8
+#define BUTTONPIN   9
 
  
 RF24 radio(CEPIN, CSNPIN);
 DHT dht(DHTPIN, DHTTYPE);
 LiquidCrystal_I2C  lcd(I2CLOCATION,2,1,0,4,5,6,7);
- 
+
+int buttonState = 0;
+long lastTemperatureCheck = 0;
+long lastButtonCheck = 0;
+long lastButtonState = 0;
+
 void setup(void)
 {
   Serial.begin(9600); 
 
   lcd.begin (16,2);
   lcd.setBacklightPin(3,POSITIVE);
-  lcd.setBacklight(HIGH);
+  lcd.setBacklight(LOW);
 
   dht.begin();
 
@@ -32,42 +38,73 @@ void setup(void)
   radio.openWritingPipe(0xF0F0F0F0E1LL);
   radio.enableDynamicPayloads();
   radio.powerUp();
+
+  pinMode(BUTTONPIN, INPUT);
+
+  lastTemperatureCheck = millis();
+  lastButtonCheck = millis();
+}
+
+void handleTemperature() {
+  int timeBetween = millis() - lastTemperatureCheck;
+  if (timeBetween >= 2000) {
+    Serial.println("Checking temperature");
+    
+    lastTemperatureCheck = millis();
+    
+    float humidity = dht.readHumidity();
+    float temperature = dht.readTemperature();
+  
+    if (isnan(humidity) || isnan(temperature)) {
+      Serial.println("Failed to read from DHT sensor!");
+      return;
+    }
+    
+    char outBuffer[15]= "";
+    char temperatureBuffer[6] = "";
+    char humidityBuffer[6] = "";
+    dtostrf(temperature, 5, 2, temperatureBuffer);
+    dtostrf(humidity, 5, 2, humidityBuffer);
+  
+    lcd.home();
+    lcd.print(temperatureBuffer);
+    lcd.setCursor (0,1);
+    lcd.print(humidityBuffer);
+  
+    sprintf(outBuffer,"|%s|%s|", temperatureBuffer, humidityBuffer);
+  
+  
+    Serial.print("Sending: ");
+    Serial.println(outBuffer);
+  
+    // transmit and increment the counter
+    radio.write(outBuffer, strlen(outBuffer));
+
+    Serial.println("Sent");
+  }
+}
+
+void handleButton() {
+  int timeBetween = millis() - lastButtonCheck;
+  buttonState = digitalRead(BUTTONPIN);
+  
+  if (buttonState != lastButtonState) {
+    lastButtonState = buttonState;
+    
+    if (buttonState == HIGH) {
+      lcd.setBacklight(HIGH);
+      Serial.println("Button pressed");
+    } else {
+      lcd.setBacklight(LOW);
+      Serial.println("Button unpressed");
+    }
+  }
 }
  
 void loop(void)
 {
-  delay(2000);
+  handleTemperature();
+  handleButton();
   
-  float humidity = dht.readHumidity();
-  float temperature = dht.readTemperature();
-
-  if (isnan(humidity) || isnan(temperature)) {
-    Serial.println("Failed to read from DHT sensor!");
-    return;
-  }
-  
-  // 32 bytes is maximum payload
-  char outBuffer[15]= "";
-  char temperatureBuffer[6] = "";
-  char humidityBuffer[6] = "";
-  dtostrf(temperature, 5, 2, temperatureBuffer);
-  dtostrf(humidity, 5, 2, humidityBuffer);
-
-  lcd.home();
-  lcd.print(temperatureBuffer);
-  lcd.setCursor (0,1);
-  lcd.print(humidityBuffer);
- 
-
-  // pad numbers and convert to string
-  //sprintf(outBuffer,"%d.2",temperature);
-
-  sprintf(outBuffer,"|%s|%s|", temperatureBuffer, humidityBuffer);
-
-
-  Serial.print("Sending: ");
-  Serial.println(outBuffer);
-
-  // transmit and increment the counter
-  radio.write(outBuffer, strlen(outBuffer));
+  delay(100);
 }
